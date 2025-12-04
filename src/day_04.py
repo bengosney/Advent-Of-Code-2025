f0209ee8fd0465912ed5038588c0f00d969d6737
Ben Gosney <bengosney@googlemail.com>
2025-12-04 16:14:56 +0000

    Day 04 - Part 02

--- src/day_04.py
+++ src/day_04.py
@@ -16,6 +16,19 @@ def draw_grid(grid: dict[tuple[int, int], str]) -> None:
         print(row)
 
 
+def neighbors(x: int, y: int) -> list[tuple[int, int]]:
+    return [
+        (x, y + 1),
+        (x, y - 1),
+        (x + 1, y),
+        (x - 1, y),
+        (x - 1, y + 1),
+        (x - 1, y - 1),
+        (x + 1, y + 1),
+        (x + 1, y - 1),
+    ]
+
+
 def part_1(puzzle: str) -> int:
     grid = defaultdict(lambda: ".")
 
@@ -29,16 +42,7 @@ def part_1(puzzle: str) -> int:
     for y in range(max_y + 1):
         for x in range(max_x + 1):
             if grid[(x, y)] == "@":
-                adjacent = [
-                    grid[(x, y + 1)],
-                    grid[(x, y - 1)],
-                    grid[(x + 1, y)],
-                    grid[(x - 1, y)],
-                    grid[(x - 1, y + 1)],
-                    grid[(x - 1, y - 1)],
-                    grid[(x + 1, y + 1)],
-                    grid[(x + 1, y - 1)],
-                ]
+                adjacent = [grid[n] for n in neighbors(x, y)]
                 if adjacent.count("@") < 4:
                     total += 1
 
@@ -46,7 +50,30 @@ def part_1(puzzle: str) -> int:
 
 
 def part_2(puzzle: str) -> int:
-    pass
+    grid = defaultdict(lambda: ".")
+
+    for y, line in enumerate(puzzle.splitlines()):
+        for x, char in enumerate(line.strip()):
+            grid[(x, y)] = char
+
+    max_x = max(x for x, _ in grid.keys())
+    max_y = max(y for _, y in grid.keys())
+    total = 0
+    previous_total = -1
+    while total != previous_total:
+        previous_total = total
+        to_move = []
+        for y in range(max_y + 1):
+            for x in range(max_x + 1):
+                if grid[(x, y)] == "@":
+                    adjacent = [grid[n] for n in neighbors(x, y)]
+                    if adjacent.count("@") < 4:
+                        to_move.append((x, y))
+                        total += 1
+        for x, y in to_move:
+            grid[(x, y)] = "."
+
+    return total
 
 
 # -- Tests
@@ -70,9 +97,9 @@ def test_part_1() -> None:
     assert part_1(test_input) == 13
 
 
-# def test_part_2() -> None:
-#     test_input = get_example_input()
-#     assert part_2(test_input) is not None
+def test_part_2() -> None:
+    test_input = get_example_input()
+    assert part_2(test_input) == 43
 
 
 @no_input_skip
@@ -81,10 +108,10 @@ def test_part_1_real() -> None:
     assert part_1(real_input) == 1587
 
 
-# @no_input_skip
-# def test_part_2_real() -> None:
-#     real_input = read_input(__file__)
-#     assert part_2(real_input) is not None
+@no_input_skip
+def test_part_2_real() -> None:
+    real_input = read_input(__file__)
+    assert part_2(real_input) == 8946
 
 
 # -- Main
