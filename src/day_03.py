9a7886ab3b362d7f2770f7bdf46b82763eb8d733
Ben Gosney <bengosney@googlemail.com>
2025-12-04 08:40:13 +0000

    Day 03 - Part 01

---
+++ src/day_03.py
@@ -0,0 +1,57 @@
+from utils import no_input_skip, read_input
+
+
+def part_1(puzzle: str) -> int:
+    total = 0
+    for line in puzzle.splitlines():
+        battries = [int(x) for x in line.strip()]
+        first = max(battries[:-1])
+        second = max(battries[battries.index(first) + 1 :])
+        total += int(f"{first}{second}")
+
+    return total
+
+
+def part_2(puzzle: str) -> int:
+    pass
+
+
+# -- Tests
+
+
+def get_example_input() -> str:
+    return """987654321111111
+811111111111119
+234234234234278
+818181911112111"""
+
+
+def test_part_1() -> None:
+    test_input = get_example_input()
+    assert part_1(test_input) == 357
+
+
+# def test_part_2() -> None:
+#     test_input = get_example_input()
+#     assert part_2(test_input) is not None
+
+
+@no_input_skip
+def test_part_1_real() -> None:
+    real_input = read_input(__file__)
+    assert part_1(real_input) == 17432
+
+
+# @no_input_skip
+# def test_part_2_real() -> None:
+#     real_input = read_input(__file__)
+#     assert part_2(real_input) is not None
+
+
+# -- Main
+
+if __name__ == "__main__":
+    real_input = read_input(__file__)
+
+    print(f"Part1: {part_1(real_input)}")
+    print(f"Part2: {part_2(real_input)}")
